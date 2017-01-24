fe7f673edf6f240cda820d52f6a60bce29e24d6f
DAO AN <langtudatinh117@gmail.com>
2017-01-24 21:01:49 +0700

    build fortune function

--- meadowlark.js
+++ meadowlark.js
@@ -1,3 +1,5 @@
+var fortune = require('./lib/fortune.js');
+
 var express = require('express');
 
 var app = express();
@@ -6,13 +8,6 @@ var handlebars = require('express-handlebars').create({ defaultLayout: 'main' })
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-var fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-];
 
 app.set('port', process.env.PORT || 3000);
 
@@ -23,8 +18,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
-    res.render('about', {fortunes: randomFortune});
+    res.render('about', { fortunes: fortune.getFortune() });
 })
 
 app.use(function (req, res, next) {
